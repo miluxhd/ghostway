ae6a734149218fed0a9f234a1688711536ea2364
Milad Zangeneh <miluxhd@gmail.com>
2025-06-03 21:28:17 +0200

    change http port to 80 and improve readme

--- client/config.py
+++ client/config.py
@@ -3,10 +3,10 @@ import logging
 
 # Environment variables
 TCP_PORT = int(os.getenv('TCP_PORT', 8001))
-RESPONSE_HTTP_PORT = int(os.getenv('RESPONSE_HTTP_PORT', 9001)) # Internal port client listens on
+RESPONSE_HTTP_PORT = int(os.getenv('RESPONSE_HTTP_PORT', 80)) # Changed default to 80
 # Use service names for Docker Compose compatibility by default
-GHOSTWAY_SERVER_URL = os.getenv('GHOSTWAY_SERVER_URL', f'http://ghostway-server:{os.getenv("TARGET_HTTP_PORT", 8002)}') 
-GHOSTWAY_CLIENT_CALLBACK_BASE_URL = os.getenv('GHOSTWAY_CLIENT_CALLBACK_BASE_URL', f'http://ghostway-client:{RESPONSE_HTTP_PORT}') # Client's public callback URL
+GHOSTWAY_SERVER_URL = os.getenv('GHOSTWAY_SERVER_URL', 'http://ghostway-server:80') # Changed default to port 80 for server
+GHOSTWAY_CLIENT_CALLBACK_BASE_URL = os.getenv('GHOSTWAY_CLIENT_CALLBACK_BASE_URL', f'http://ghostway-client:{RESPONSE_HTTP_PORT}') # Will use new RESPONSE_HTTP_PORT default (80)
 
 # Gzip Configuration
 GZIP_ENABLED = os.getenv('GZIP_ENABLED', 'true').lower() == 'true'
